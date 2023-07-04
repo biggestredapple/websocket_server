b11ca6a2d43e066bc0f0f169af87ae5085a02651
Apple Man <happy3guy@gmail.com>
2023-07-04 19:41:31 +0300

    feat: add channel & controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  get 'pages/home'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  post "/hello", to: "pages#hello"
+  root "pages#home"
 end
